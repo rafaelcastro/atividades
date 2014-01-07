aa57f6e0371e5bee196ddc5581b424358e278f04
rafaelcastro <rafael_bronkeiro@hotmail.com>
2014-01-07 18:37:46 -0200

    Create menu.php

---
+++ cadastro/menu.php
@@ -0,0 +1,8 @@
+<a href="index.php">Listar</a>
+&nbsp;&nbsp;
+<a href="form_cadastrar.php">Cadastrar</a>
+&nbsp;&nbsp;
+<a href="form_remover.php">Remover</a>
+<br/><br/>
+<a href="form_editar.php">Editar</a>
+<br/><br/>
