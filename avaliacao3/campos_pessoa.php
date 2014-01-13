60d0d2301e8e97f246d008a8b6e77e545c4256e5
rafaelcastro <rafael_bronkeiro@hotmail.com>
2014-01-13 17:53:14 -0200

    Create campos_pessoa.php

---
+++ avaliacao3/campos_pessoa.php
@@ -0,0 +1,34 @@
+
+Nome do produto
+<br/>
+<input type="text" name="nome" />
+<br/>
+<br/>
+
+Cor do produto:
+<br/>
+<input type="radio" name="sexo" value="Azul" checked="checked" /> 
+<label>Azul</label>
+<input type="radio" name="sexo" value="Ciano"/>
+Ciano
+<input type="radio" name="sexo" value="Vermelho"/>
+Vermelho
+<br/>
+<br/>
+<input type="checkbox" name="aceito" value="Sim" />
+Produto novo 
+<br/>
+<br/>
+opções de entrega: <br/>
+<select name="estado">
+    <option>5 Dias</option>
+    <option>10 Dias</option>
+    <option>15 Dias</option>
+</select>
+<br/>
+<br/>
+
+Detalhe do produto
+<br/>
+<textarea name="observacoes" MAXLENGTH="180">Escreva aque o detalhe do produto</textarea>
+<br/>
