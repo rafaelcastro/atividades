df518dffe2eda21b6fed7aca6fc096f2da99e9f8
rafaelcastro <rafael_bronkeiro@hotmail.com>
2014-01-13 18:02:06 -0200

    Create menu.php

---
+++ avaliacao3/menu.php
@@ -0,0 +1,12 @@
+<center>
+<fieldset>
+<a href="index.php">Listar</a>
+&nbsp;&nbsp;
+<a href="form_cadastrar.php">Cadastrar</a>
+&nbsp;&nbsp;
+<a href="form_editar.php">Editar</a>
+&nbsp;&nbsp;
+<a href="form_remover.php">Remover</a>
+<br/><br/>
+</fieldset>
+</center>
